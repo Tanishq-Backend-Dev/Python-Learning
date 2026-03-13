9277ae1620a0719dc137059d90355699529873bb
Tanishq-Backend-Dev <tanishq02thakur@gmail.com>
2026-03-13 15:16:41 +0530

    Practice

--- codingPractice.py
+++ codingPractice.py
@@ -4,35 +4,35 @@
 
 # Q.1.Hello World program
 
-# print("Hello World !")
+# print("Hello World!")
 
 # Q.2.Take user input and print it
 
-# age = int(input("Please enter your age:- "))
-# print(age)
+# name = input("Enter your name - ")
+# print(name)
 
 # Q.3.Check if a number is even or odd
 
-# num = int(input("Enter a number:- "))
+# num = int(input("Enter a number - "))
 # if num%2==0:
-#     print(num,"is a even number")
+#     print(num,"is a even number.")
 # else:
-#     print(num,"is a odd number")
+#     print(num,"is a odd number.")
 
 # Q.4.Find the largest of two numbers
 
-# num1 = int(input("Enter a number :- "))
-# num2 = int(input("Enter a number :- "))
+# num1 = int(input("Enter a number - "))
+# num2 = int(input("Enter a number - "))
 # if num1>num2:
-#     print(num1,"is greater than",num2)
+#     print(num1,"is a greater number than",num2)
 # else:
-#     print(num2,"is greater than",num1)
+#     print(num2,"is greater number than",num1)
 
 # Q.5.Find the largest of three numbers
 
-# num1 = int(input("Enter a number :- "))
-# num2 = int(input("Enter a number :- "))
-# num3 = int(input("Enter a number :- "))
+# num1 = int(input("Enter a number - "))
+# num2 = int(input("Enter a number - "))
+# num3 = int(input("Enter a number - "))
 # if num1>num2 and num1>num3:
 #     print(num1,"is greater than",num2,"and",num3)
 # elif num2>num1 and num2>num3:
@@ -42,18 +42,20 @@
 
 # Q.6.Swap two variables
 
-# a = int(input("Enter a number:- "))
-# b = int(input("Enter a number:- "))
+# a = int(input("Enter a number : "))
+# b = int(input("Enter a number : "))
+# print("a -",a)
+# print("b -",b)
 # print("After swapping")
 # temp = a
 # a = b
 # b = temp
-# print(a)
-# print(b)
+# print("a -",a)
+# print("b -",b)
 
 # Q.7.Calculate factorial of a number
 
-# num = int(input("Enter a number:- "))
+# num = int(input("Enter a number - "))
 # fac = 1
 # for i in range(1,num+1):
 #     fac = fac * i
@@ -61,34 +63,34 @@
 
 # Q.8.Generate Fibonacci series
 
-# n = int(input("Enter a number:- "))
+# n = int(input("Enter a number :- "))
 # a = 0
 # b = 1
-# if n <= 0:
-#     print("Please enter a number greater than 0")
-# elif n == 1:
+# if n==0:
+#     print("Please enter a positive number.")
+# elif n==1:
 #     print(a)
 # else:
 #     print(a)
 #     print(b)
 #     for i in range(2,n):
-#         c = a + b
+#         c = a+b
 #         a = b
 #         b = c
 #         print(c)
 
 # Q.9.Check if a number is prime
 
-# num = int(input("Enter a number:- "))
-# if num<=1:
+# n = int(input("Enter a number :- "))
+# if n<=1:
 #     print("Please enter a number greater than 1")
 # else:
-#     for i in range(2,num):
-#         if num%i==0:
-#             print(num,"is not a prime number")
+#     for i in range(2,n):
+#         if n%i==0:
+#             print(n,"is not a prime number.")
 #             break
 #     else:
-#         print(num,"is a prime number")
+#         print(n,"is a prime number.")
 
 # Q.10.Print all prime numbers in a range
 
@@ -96,11 +98,11 @@
 
 # Q.11.Reverse a number
 
-# num = int(input("Enter a number:- "))
+# num = int(input("Enter a number :- "))
 # str_num = str(num)
 # reversed_str_num = str_num[::-1]
 # reversed_num = int(reversed_str_num)
-# print("Your number in reverse order is:-",reversed_num)
+# print("Your number in reverse order is -",reversed_num)
 
 # Q.12.Check palindrome (number & string)
 
@@ -108,22 +110,22 @@
 
 # Q.13.Count digits in a number
 
-# num = int(input("Enter a number:- "))
-# str_num = str(num)
+# num = int(input("Enter a number :- "))
 # count = 0
+# str_num = str(num)
 # for i in str_num:
 #     count = count + 1
 # print("The number of digits in your number is",count)
 
 # Q.14.Sum of digits of a number
 
-# num = int(input("Enter a number:- "))
+# num = int(input("Enter a number :- "))
 # sum = 0
 # while num>0:
-#     digit = num%10
+#     digit = num % 10
 #     sum = sum + digit
 #     num = num // 10
-# print("The sum of a digits of your number is",sum)
+# print("The sum of digits of your number is",sum)
 
 # Q.15.Find ASCII value of a character
 
@@ -131,18 +133,18 @@
 
 # Q.16.Simple calculator using if-else
 
-# print("---------- Calculator ----------")
-# operation = input("Which operation you want to perform? - Addition, Subtraction, Multiplication and Division:- ").lower()
+# ------------------ Calculator# ------------------ #
+# operation = input("What type of operation you want to perform? - Addition, Subtraction, Multiplication and Division - ").lower()
 # num1 = int(input("Enter a number:- "))
 # num2 = int(input("Enter a number:- "))
 # if operation == "addition":
-#     print("result is",num1+num2)
+#     print("Result is",num1+num2)
 # elif operation == "subtraction":
-#     print("result is",num1 - num2)
+#     print("Result is",num1-num2)
 # elif operation == "multiplication":
-#     print("result is",num1*num2)
+#     print("Result is",num1*num2)
 # elif operation == "division":
-#     print("result is", num1/num2)
+#     print("Result is",num1/num2)
 
 # Q.17.Area of circle / rectangle / triangle
 
@@ -158,13 +160,13 @@
 
 # Q.20.Print multiplication table
 
-# num = int(input("Enter a number:- "))
+# num = int(input("Enter a number :- "))
 # for i in range(1,11):
 #     print(num*i)
 
 # Q.21.Count vowels and consonants in a string
 
-# name = input("Enter your name:- ")
+# name = input("Please enter your name :- ")
 # vowels = 0
 # consonants = 0
 # for i in name:
@@ -172,22 +174,22 @@
 #         vowels = vowels + 1
 #     else:
 #         consonants = consonants + 1
-# print("The number of vowels in your sentence is",vowels)
+# print("The number of vowels in your sentence is", vowels)
 # print("The number of consonants in your sentence is",consonants)
 
 # Q.22.Reverse a string
 
-# fullName = input("Enter your name:- ")
-# reversed_fullName = fullName[::-1]
-# print("Your name in reverse order is :",reversed_fullName)
+# fullName = input("Enter your name :- ")
+# reversed_name = fullName[::-1]
+# print("Your name in reversed order is",reversed_name)
 
 # Q.23.Find string length without using len() [including spaces]
 
-# name = input("Enter your name:- ")
+# fullName = input("Please enter your full name:- ")
 # count = 0
-# for i in name:
+# for i in fullName:
 #     count = count + 1
-# print("Length of your string is:-",count)
+# print("The length of your string is -",count)
 
 # Q.24.Check anagram
 
@@ -199,14 +201,14 @@
 
 # Q.26.Count words in a sentence
 
-# fullName = input("Please enter your full name:- ")
+# fullName = input("Please enter yor full name:- ")
 # count = 0
 # for i in fullName:
 #     if i == " ":
 #         continue
 #     else:
 #         count = count + 1
-# print("The number of words in your sentence is:-",count)
+# print("The number of words in your sentence is",count)
 
 # Q.27.Remove spaces from a string
 
@@ -242,23 +244,23 @@
 
 # Q.34.Sort list without using sort()
 
-# def sort(nums):
-#     for i in range(len(nums)-1,0,-1):
+# def sort(marks):
+#     for i in range(len(marks)-1,0,-1):
 #         for j in range(i):
-#             if nums[j]>nums[j+1]:
-#                 temp = nums[j]
-#                 nums[j] = nums[j+1]
-#                 nums[j+1] = temp
-# nums = [1,10,2,9,3,8,4,7,5,6]
-# sort(nums)
-# print(nums)
+#             if marks[j]>marks[j+1]:
+#                 temp = marks[j]
+#                 marks[j] = marks[j+1]
+#                 marks[j+1] = temp
+# marks = [10,12,8,4,17,12]
+# sort(marks)
+# print(marks)
 
 # Q.35.Merge two lists
 
 # num1 = [1,2,3,4,5]
 # num2 = [6,7,8,9,10]
-# num3 = num1 + num2
-# print("Merged list is",num3)
+# merged_num = num1+num2
+# print("Merged list is",merged_num)
 
 # Q.36.Rotate a list
 
@@ -266,19 +268,19 @@
 
 # Q.37.Find common elements in two lists
 
-# num1 = [1,2,3,4,5,6,7,8,9,10]
-# num2 = [1,3,5,7,9,11,13,15,17,19]
+# list1 = [1,2,3,4,5,6,7,8,9,10]
+# list2 = [1,3,5,7,9,11,13,15,17,19]
 # common_ele = []
-# for i in num1:
-#     if i in num2:
+# for i in list1:
+#     if i in list2:
 #         common_ele.append(i)
-# print("List with common elements is",common_ele)
+# print("The list with common elements is",common_ele)
 
 # Q.38.Count occurrences of elements
 
-# marks = (11,17,14,10,17)
-# occuurence = marks.count(17)
-# print("The number od people who got mark 17 is",occuurence)
+# marks = (12,14,16,12,11)
+# marks_occurence = marks.count(12)
+# print("The number of students who got marks-12 is",marks_occurence)
 
 # Q.39.Flatten a nested list
 
@@ -334,13 +336,13 @@
 
 # Q.51.Write recursive factorial function
 
-# def factorial(num):
-#     if num == 1:
+# def factorial(n):
+#     if n == 1:
 #         return 1
-#     return num * factorial(num-1)
+#     return n * factorial(n-1)
 
-# print(factorial(6))
 # print(factorial(5))
+# print(factorial(4))
 
 # Q.52.Recursive Fibonacci
 
@@ -349,20 +351,21 @@
 # Q.53.Lambda function examples
 
 # sum = lambda a,b : a+b
-# print(sum(3,9))
+# print(sum(3,4))
 
 # Q.54.Map, filter, reduce usage
 
 # from functools import reduce
+
 # nums = [1,2,3,4,5,6,7,8,9,10]
 
-# even = list(filter(lambda a : a%2==0,nums))
-# doubles = list(map(lambda a : a*2, nums))
-# sum = reduce(lambda a,b : a+b, nums)
+# even = list(filter(lambda a : a%2==0, nums))
+# double = list(map(lambda a : a*2, nums))
+# sum = reduce(lambda a,b : a+b,nums)
 
-# print("Filter:-",even)
-# print("Map:-",doubles)
-# print("Reduce:-",sum)
+# print("filter:- ",even)
+# print("map:- ",double)
+# print("reduce:- ",sum)
 
 # Q.55.Function to check Armstrong number
 
@@ -382,21 +385,23 @@
 
 # Q.59.Generator function example
 
-# def TopNSquares(num):
+# def topSquaresGenerator(limit):
 #     i = 1
-#     while i<=num:
-#         sq = i * i
-#         i = i + 1
-#         yield sq
-# value = TopNSquares(7)
-# print(value.__next__())
-# print(value.__next__())
-# print(value.__next__())
-# print(value.__next__())
+#     while i<=limit:
+#         squares = i * i
+#         yield squares
+#         i += 1
+# result = topSquaresGenerator(5)
+# print(result.__next__())
+# print(result.__next__())
+# print(result.__next__())
+# print(result.__next__())
+# print(result.__next__())
+# print(result.__next__())
 
 # Q.60.Custom iterator
 
-# class TopSquares:
+# class SquareIterator():
 #     def __init__(self,limit):
 #         self.num = 1
 #         self.limit = limit
@@ -404,16 +409,17 @@
 #         return self
 #     def __next__(self):
 #         if self.num <= self.limit:
-#             squares = self.num * self.num
+#             val = self.num * self.num
 #             self.num += 1
-#             return squares
+#             return val
 #         raise StopIteration
-# t1 = TopSquares(4)
-# print(t1.__next__())
-# print(t1.__next__())
-# print(t1.__next__())
-# print(t1.__next__())
-# print(t1.__next__())
+# s = SquareIterator(5)
+# print(s.__next__())
+# print(s.__next__())
+# print(s.__next__())
+# print(s.__next__())
+# print(s.__next__())
+# print(s.__next__())
 
 # File Handling & Exceptions
 
@@ -449,17 +455,17 @@
 #Zero Division Error
 
 # try:
-#     a = int(input("Enter a number:- "))
-#     b = int(input("Enter a number:- "))
-#     print("Result is",a/b)
+#     a = int(input("Enter a number :- "))
+#     b = int(input("Enter a number :- "))
+#     print("Result is", a/b)
 # except ZeroDivisionError as z:
-#     print("Divisor cannot be zer",z)
+#     print("Number can't be zero",z)
 # except ValueError as v:
-#     print("Invalid value",v)
+#     print("Please enter a valid value",v)
 # except Exception as e:
-#     print("Something went wrong",e)
+#     print("Error",e)
 # finally:
-#     print("Completed")
+#     print("Operation Completed")
 
 # Q.69.Logging errors to a file
 
@@ -475,19 +481,21 @@
 
 # Q.71.Create a class and object
 
-# class Employee:
-#     def __init__(self,name,age,salary):
-#         self.name = name
-#         self.age = age
-#         self.salary = salary
+# class Employee():
+#     def __init__(self):
+#         self.name = "Tanishq"
+#         self.age = 21
+#         self.salary = 75000
 #         self.company = "HSBC"
-#     def organization(self):
-#         return f"{self.name} is working in {self.company}"
-# e1 = Employee("Tanishq", 25, 68000)
+#     def tenure(self):
+#         return f"{self.name} is working in {self.company} for more than 2 years"
+
+# e1 = Employee()
 # print(e1.name)
 # print(e1.age)
 # print(e1.salary)
-# print(e1.organization())
+# print(e1.company)
+# print(e1.tenure())
 
 # Q.72.Constructor and destructor
 
@@ -497,15 +505,14 @@
 
 # class Animal:
 #     def name(self):
-#         print("My name is Bruno")
-
+#         return "My name is bruno"
 # class Dog(Animal):
 #     def age(self):
-#         print("I am 5 years old")
+#         return "I am 5 years old"
 
 # d1 = Dog()
-# d1.name()
-# d1.age()
+# print(d1.name())
+# print(d1.age())
 
 # Q.74.Multiple inheritance
 
@@ -513,16 +520,14 @@
 
 # Q.75.Method overriding
 
-# class Vinod:
+# class Vinod():
 #     def phone(self):
-#         print("I have samsung phone")
-
+#         print("I have Samsung Phone")
 # class Tanishq(Vinod):
 #     def phone(self):
 #         print("I have an iPhone")
-
-# t1 = Tanishq()
-# t1.phone()
+# t = Tanishq()
+# t.phone()
 
 # Q.76.Encapsulation (private variables)
 
@@ -547,14 +552,14 @@
 #         m1 = self.m1 + other.m1
 #         m2 = self.m2 + other.m2
 #         m3 = self.m3 + other.m3
-#         return (m1,m2,m3)
+#         return m1,m2,m3
 #     def __sub__(self,other):
 #         m1 = self.m1 - other.m1
 #         m2 = self.m2 - other.m2
 #         m3 = self.m3 - other.m3
-#         return (m1,m2,m3)
-# s1 = Student(10,15,20)
-# s2 = Student(5,10,15)
+#         return m1,m2,m3
+# s1 = Student(5,10,15)
+# s2 = Student(15,20,25)
 # s3 = s1-s2
 # print(s3)
 
@@ -566,32 +571,34 @@
 
 # Q.81.Binary search implementation
 
-# def binarySearch(nums,val):
+# def binarySearch(nums,ele):
 #     l = 0
 #     u = len(nums)-1
 #     while l<=u:
 #         mid = (l+u)//2
-#         if nums[mid]==val:
+#         if nums[mid] == ele:
 #             return mid
 #         else:
-#             if nums[mid]<val:
+#             if nums[mid] < ele :
 #                 l = mid + 1
 #             else:
 #                 u = mid - 1
 #     return -1
+
 # nums = [1,2,3,4,5,6,7,8,9,10]
-# print(binarySearch(nums,10))
+# print(binarySearch(nums,4))
 
 # Q.82.Linear search
 
-# def linearSearch(nums,val):
-#     for i in range(0,len(nums)):
-#         if nums[i]==val:
-#             return i
-#     return -1
+# def linearSearch(nums,ele):
+#     for i in range(len(nums)):
+#         if nums[i]==ele:
+#             return ele,"found at",i
+#     else:
+#         return -1
 
-# nums = [1,8,3,6,4,5,67,99]
-# print(linearSearch(nums,889))
+# nums = [11,21,34,32,1,2,3,3,4]
+# print(linearSearch(nums,4))
 
 # Q.83.Bubble sort
 
@@ -650,19 +657,18 @@
 # Q.91.Multithreading example
 
 # from threading import Thread
-# from time import *
+# from time import sleep
 
 # class Hello(Thread):
 #     def run(self):
-#         for i in range(6):
+#         for i in range(1,7):
 #             print("Hello")
-#             sleep(2)
-
+#             sleep(3)
 # class Hi(Thread):
 #     def run(self):
-#         for i in range(4):
+#         for i in range(1,4):
 #             print("Hi")
-#             sleep(3)
+#             sleep(2)
 
 # t1 = Hello()
 # t2 = Hi()
